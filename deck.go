337c0bdbc26a992842a0b6a2a55b90646fa84fe3
Fellipe Wurthmann <felipe.wurthmann@gmail.com>
2020-12-27 23:15:30 -0300

    feature(deck): create functions for handling deck

---
+++ deck.go
@@ -0,0 +1,26 @@
+package main
+
+import "fmt"
+
+type deck []string
+
+func newDeck() deck {
+	cards := deck{}
+
+	cardSuits := []string{"Spades", "Diamonds", "Hearts", "Clubs"}
+	cardValues := []string{"Ace", "Two", "Three", "Four"}
+
+	for _, suits := range cardSuits {
+		for _, values := range cardValues {
+			cards = append(cards, values+" of "+suits)
+		}
+	}
+
+	return cards
+}
+
+func (d deck) print() {
+	for index, card := range d {
+		fmt.Println(index, card)
+	}
+}
